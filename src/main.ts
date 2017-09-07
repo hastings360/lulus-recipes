539daaf3e0e88a38d3afbe788122fd14efefcda5
Larry <hastings360@gmail.com>
2017-09-07 16:45:54 -0400

    dsfg

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,3 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-
-
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
