0f9286f65f70cde5f4b6a8147059a04899295359
Larry <hastings360@gmail.com>
2017-09-29 15:45:27 -0400

    update tmp file

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: 'tmp-uploads/'})
+const upload = multer({dest: '/tmp/'})
 
 const nodemailer = require('nodemailer');//mail API
 const MongoClient = require('mongodb').MongoClient;//db API
