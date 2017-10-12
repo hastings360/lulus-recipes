9a49c90044553bb8579655ae93817b03c7f2cdce
Larry <hastings360@gmail.com>
2017-10-12 15:34:49 -0400

    adsf

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,8 +14,8 @@ export class SearchComponent implements OnInit {
 
   public queryText: String;
   public searchForm: FormGroup;
-  public queryResults: Meal[];
   public data: any;
+  private resultsList = document.getElementById("results");
   
 
   constructor(fb: FormBuilder,private dbTalker: DbTalkerService) { 
@@ -31,10 +31,15 @@ export class SearchComponent implements OnInit {
     
     let queryToJSON = JSON.parse(JSON.stringify(incomingQueryObject));
     let queryRequest = queryToJSON.query;
+    let queryResults: Meal[];
+    let resultsList = document.getElementById("results");
     
     this.dbTalker.QuerySearchMeals(queryRequest,this.data,val => {
-      console.log(val);
-      return this.queryResults = val;
+      return queryResults = val;
     })
+
+    for(let y in queryResults){
+      console.log(y);
+    }
   }
 }
