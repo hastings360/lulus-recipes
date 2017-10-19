51b97b086df29cbe3d56bce05139ecfc1de58b97
Larry <hastings360@gmail.com>
2017-10-19 14:47:22 -0400

    asdf

--- src/app/meal-displays/meal-info-view/meal-info-view.component.ts
+++ src/app/meal-displays/meal-info-view/meal-info-view.component.ts
@@ -11,10 +11,14 @@ export class MealInfoViewComponent implements OnInit {
 
 @Input() specifiedMeal: Meal;
 
-public liked: any = localStorage.getItem(this.specifiedMeal._id);
+public liked: boolean = false;
 
   constructor() { 
-    
+    if(localStorage.getItem(this.specifiedMeal._id)){
+      this.liked = true;
+    }else{
+      this.liked = false;
+    }
   }
 
   ngOnInit() {
@@ -22,7 +26,7 @@ public liked: any = localStorage.getItem(this.specifiedMeal._id);
 
   addLike(){
     localStorage.setItem(this.specifiedMeal._id,'liked');
-    console.log(this.specifiedMeal._id);
+    
   }
 
 }
